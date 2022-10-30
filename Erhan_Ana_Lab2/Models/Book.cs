cc5c262956ca522aad2fe0ee617749e08be4f759
Ana-Maria Erhan <Ana.Erhan@cloudflight.io>
2022-10-30 23:44:17 +0200

    Add project files.

---
+++ Erhan_Ana_Lab2/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Erhan_Ana_Maria_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public decimal Price { get; set; }
+        public int? AuthorID { get; set; }
+        // ensure non-nullable property -> ?
+        public Author? Author { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
